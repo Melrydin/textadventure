aeb05cb96b23c2aeac4c82f09702d5b272352fc7
clemens2107 <clemens2107@live.de>
2020-12-07 16:19:45 +0100

    - Add equipment properties system - Bug Fixing

--- Textadventure/items/equipment_properties.py
+++ Textadventure/items/equipment_properties.py
@@ -1,1 +1,34 @@
 # -*- coding: utf-8 -*-
+
+# name, min, max, classe_speziefisch, item vergleich, item speziefische stats
+
+main_properties_item_stats = ["helm", "chest_armor", "belt",
+                              "boot", "glove", "pant","shield",
+                              "shoulder","amulet", "ring", "sword", "axe", "mace",
+                              "spear", "tow_handed_sword", "tow_handed_axe",
+                              "tow_handed_mace"]
+
+main_properties = [["Dexterity", 5, 100, None, True, main_properties_item_stats],
+                   ["Strength", 5, 100, None, True, main_properties_item_stats],
+                   ["Intelligence", 5, 100, None, True, main_properties_item_stats],
+                   ["Vitality", 5, 100, None, True, main_properties_item_stats]]
+
+primary_properties = [["Critical hit Chance", 5, 10, None, True, ["glove","amulet", "ring","helm"],
+                      ["Critical hit Danage", 25, 50, None, True, [""]],
+                      ["Dodge Chance", 0, 10, None, True, [""]],
+                      ["Armor", 100, 250, None, True, [""]],
+                      ["Total Life Bonus", 10, 15, None, True, [""],
+                      ["Resource Cost Reduction", 8, 10, None, True, [""]]
+
+                       
+sekunder_properties = [["Physical Resistance", 50, 100, None, True, [""]],
+                       ["Cold Resistance", 50, 100, None, True, [""]],
+                       ["Fire Resistance", 50, 100, None, True, [""]],
+                       ["Lightning Resistance", 50, 100, None, True, [""]],
+                       ["Poison Resistance", 50, 100, None, True, [""]],
+                       ["Arcan/Holy Resistance", 50, 100, None, True, [""]],
+                       ["Life per Hit", 50, 100, None, True, [""]],
+                       ["Life per Kill", 50, 100, None, True, [""]],
+                       ["Gold Find", 50, 100, None, True, [""]],
+                       ["Magic Find", 50, 100, None, True, [""]],
+                       ["Bonus Experience", 50, 100, None, True, [""]]]
--- Textadventure/items/player_stats.py
+++ Textadventure/items/player_stats.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
-player_stats_dic = {"level": 0,
+from enemies import player
+
+player_stats_dic = {"name": player.name, 
+                    "level": 0,
+                    "live": player.hp,
                     "strength": 0,
-                    "intellligence": 0,
+                    "intelligence": 0,
                     "dexterity": 0,
                     "vitality": 0,
                     "critical_hit_chance": 0,
@@ -15,7 +19,6 @@ player_stats_dic = {"level": 0,
                     "lightning_resistance": 0,
                     "poison_resistance": 0,
                     "arcan_holy_resistance": 0,
-                    "Life": 0,
                     "total_life_bonus": 0,
                     "life_per_hit": 0,
                     "life_per_kill": 0,
@@ -23,3 +26,30 @@ player_stats_dic = {"level": 0,
                     "gold_find": 0,
                     "magic_find": 0,
                     "bonus_experience": 0}
+
+def print_player():
+    print(6*"-" + "Your Stats" + 6*"-")
+    print("Name: " + player_stats_dic["name"])
+    print("Level: " + player_stats_dic["level"])
+    print("Live: " + player_stats_dic["live"])
+    print("Stringth: " + player_stats_dic["strength"])
+    print("Intelligence: " + player_stats_dic["intelligence"])
+    print("Dexterity: " + player_stats_dic["dexterity"])
+    print("Vitality: " + player_stats_dic["vitality"])
+    print("Critical Hit Chance: " + player_stats_dic["critical_hit_chance"])
+    print("Critical Hit Danage: " + player_stats_dic["critical_hit_danage"])
+    print("Doge Chance: " + player_stats_dic["dodge_chance"])
+    print("Armor: " + player_stats_dic["armor"])
+    print("Physical Resistance: " + player_stats_dic["physical_resistance"])
+    print("Cold Resistance: " + player_stats_dic["cold_resistance"])
+    print("Fire Resistance: " + player_stats_dic["fire_resistance"])
+    print("Lightning Resistance: " + player_stats_dic["lightning_resistance"])
+    print("Poison Resistance: " + player_stats_dic["poison_resistance"])
+    print("Arcan Holy Resistance: " + player_stats_dic["arcan_holy_resistance"])
+    print("Total Life Bonus: " + player_stats_dic["total_life_bonus"])
+    print("Life per Hit: " + player_stats_dic["life_per_hit"])
+    print("Life per Kill: " + player_stats_dic["life_per_kill"])
+    print("Resource cost Reduction: " + player_stats_dic["resource_cost_reduction"])
+    print("Gold find: " + player_stats_dic["gold_find"])
+    print("Magic find: " + player_stats_dic["magic_find"])
+    print("Bonus Experience: " + player_stats_dic["bonus_experience"])
